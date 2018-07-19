a71aba92bf7bbce98906a88dc2d18a73d97f8da9
EC2 Default User <ec2-user@ip-172-31-21-27.us-east-2.compute.internal>
2018-07-19 06:53:47 +0000

    Display a user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/show'
+
   devise_for :users
+  #2018/07/19,using "show" action,suzuki
+  resources :users, only: [:show]
   root 'static_pages#home'
 
   match '/help',    to: 'static_pages#help',    via: 'get'
@@ -7,6 +11,8 @@ Rails.application.routes.draw do
   match '/about',   to: 'static_pages#about',   via: 'get'
   
   match '/contact', to: 'static_pages#contact', via: 'get'
+  
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
