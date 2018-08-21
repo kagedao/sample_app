4d2b5404698276f41b38868f3ddb846bd3399287
EC2 Default User <ec2-user@ip-172-31-21-27.us-east-2.compute.internal>
2018-08-21 06:27:31 +0000

    Make a Relationship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
+  has_many :reverse_relationships, foregin_key: "follower_id",
+                                   class_name: :"Relationship",
+                                   dependent: :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -7,6 +12,18 @@ class User < ApplicationRecord
          
   def feed
     Micropost.where("user_id = ?", id)
+  end
+  
+  def following?(other_user)
+    relationships.find_by(followed_id: other_user.id)
+  end
+  
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+  
+  def unfollow!(other_user)
+    relationships.find_by(followed_id: other_user.id).destroy
   end
   
   #validates :name, presence: true, length: { maximum: 50 }
